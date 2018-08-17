6ce6b682256f6754b4f58180b05d28417704a090
camusicjunkie <33888807+camusicjunkie@users.noreply.github.com>
2018-08-17 11:47:34 -0600

    Add files via upload

---
+++ HPBios.psm1
@@ -0,0 +1,88 @@
+﻿function Set-HPBiosPassword {
+    [cmdletbinding()]
+    param (
+        [Parameter(ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [string[]] $ComputerName,
+
+        [Parameter(Mandatory)]
+        [string] $CurrentPassword,
+
+        [Parameter(Mandatory)]
+        [string] $NewPassword
+    )
+
+    begin {
+        $arguments = @{
+            Name = 'Setup Password'
+            Password = "<utf-16/>$CurrentPassword"
+            Value = "<utf-16/>$NewPassword"
+        }
+
+        if (-not $PSBoundParameters.ContainsKey('ComputerName')) {
+            $ComputerName = $env:COMPUTERNAME
+        }
+    }
+
+    process {
+        foreach ($computer in $ComputerName) {
+            do {
+                Write-Verbose "Connecting to $computer on Wsman"
+                $protocol = 'Wsman'
+
+                try {
+                    $option = New-CimSessionOption -Protocol $protocol
+                    $sessionParams = @{
+                        ComputerName = $computer
+                        SessionOption = $option
+                        ErrorAction = 'Stop'
+                    }
+                    $session = New-CimSession @sessionParams
+
+                    $gcimParams = @{
+                        Namespace = 'root/HP/InstrumentedBIOS'
+                        ClassName = 'HP_BIOSSettingInterface'
+                        CimSession = $session
+                    }
+                    $biosClass = Get-CimInstance @gcimParams
+
+                    Write-Verbose "Setting password on $computer"
+                    $icimParams = @{
+                        InputObject = $biosClass
+                        Arguments = $arguments
+                        MethodName = 'SetBIOSSetting'
+                    }
+                    $return = Invoke-CimMethod @icimParams
+
+                    switch ($return.return) {
+                        0 {$status = 'Success'}
+                        1 {$status = 'Not Supported'}
+                        2 {$status = 'Unspecified Error'}
+                        3 {$status = 'Timeout'}
+                        4 {$status = 'Failed'}
+                        5 {$status = 'Invalid Parameter'}
+                        6 {$status = 'Access Denied'}
+                        default {$status = "Failed: $($return.return)"}
+                    }
+
+                    $obj = [pscustomobject] @{
+                        ComputerName = $computer
+                        Status = $status
+                    }
+                    Write-Output $obj
+
+                    Write-Verbose "Closing connection to $computer"
+                    $session | Remove-CimSession
+                    $protocol = 'Stop'
+                }
+                catch {
+                    switch ($protocol) {
+                        'Wsman' {$protocol = 'Dcom'}
+                        'Dcom'  {$protocol = 'Stop'}
+                    }
+                }
+            }
+            until ($protocol = 'Stop')
+        }
+    }
+}
