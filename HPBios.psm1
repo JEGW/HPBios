e6ca545eb23df39976929da632c62eb16294430f
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2018-11-19 07:16:02 -0700

    Added more functions
    Added more functions that wrap around the HP Bios WMI classes and methods. Functionality now exists for Secure Boot and Virtualization.

--- HPBios.psm1
+++ HPBios.psm1
@@ -1,4 +1,4 @@
-﻿function Set-HPBiosPassword {
+function Set-HPBiosPassword {
     [cmdletbinding()]
     param (
         [Parameter(ValueFromPipeline,
@@ -84,5 +84,179 @@
             }
             until ($protocol = 'Stop')
         }
+    }
+}
+
+function Get-HPBiosSecureBoot {
+    [cmdletbinding()]
+    param (
+        [Parameter(ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [string[]] $ComputerName
+    )
+
+    begin {
+        if (-not $PSBoundParameters.ContainsKey('ComputerName')) {
+            $ComputerName = $env:COMPUTERNAME
+        }
+    }
+
+    process {
+        foreach ($computer in $ComputerName) {
+            do {
+                Write-Verbose "Connecting to $computer on Wsman"
+                $protocol = 'Wsman'
+
+                try {
+                    $option = New-CimSessionOption -Protocol $protocol
+                    $sessionParams = @{
+                        ComputerName = $computer
+                        SessionOption = $option
+                        ErrorAction = 'Stop'
+                    }
+                    $session = New-CimSession @sessionParams
+
+                    $gcimParams = @{
+                        Namespace = 'root/HP/InstrumentedBIOS'
+                        ClassName = 'HP_BIOSSetting'
+                        CimSession = $session
+                        Filter = "Name = 'Configure Legacy Support and Secure Boot'"
+                    }
+                    $secureBoot = Get-CimInstance @gcimParams
+
+                    $obj = [pscustomobject] @{
+                        'Name' = $secureBoot.Name
+                        'CurrentValue' = $secureBoot.currentValue
+                        'PossibleValues' = $secureBoot.possibleValues
+                        'ComputerName' = $secureBoot.PSComputerName
+                    }
+                    Write-Output $obj
+
+                    Write-Verbose "Closing connection to $computer"
+                    $session | Remove-CimSession
+                    $protocol = 'Stop'
+                }
+                catch {
+                    switch ($protocol) {
+                        'Wsman' {$protocol = 'Dcom'}
+                        'Dcom'  {$protocol = 'Stop'}
+                    }
+                }
+            }
+            until ($protocol = 'Stop')
+        }
+    }
+}
+
+function Get-HPBiosVirtualization {
+    [cmdletbinding()]
+    param (
+        [Parameter(ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [string[]] $ComputerName
+    )
+
+    begin {
+        if (-not $PSBoundParameters.ContainsKey('ComputerName')) {
+            $ComputerName = $env:COMPUTERNAME
+        }
+    }
+
+    process {
+        foreach ($computer in $ComputerName) {
+            do {
+                Write-Verbose "Connecting to $computer on Wsman"
+                $protocol = 'Wsman'
+
+                try {
+                    $option = New-CimSessionOption -Protocol $protocol
+                    $sessionParams = @{
+                        ComputerName = $computer
+                        SessionOption = $option
+                        ErrorAction = 'Stop'
+                    }
+                    $session = New-CimSession @sessionParams
+
+                    $gcimParams = @{
+                        Namespace = 'root/HP/InstrumentedBIOS'
+                        ClassName = 'HP_BIOSSetting'
+                        CimSession = $session
+                        Filter = "Name LIKE '%virtual%'"
+                    }
+                    $virtual = Get-CimInstance @gcimParams
+                    
+                    foreach ($v in $virtual) {
+                        $obj = [pscustomobject] @{
+                            'Name' = $v.Name
+                            'CurrentValue' = $v.currentValue
+                            'PossibleValues' = $v.possibleValues
+                            'ComputerName' = $v.PSComputerName
+                        }
+                        Write-Output $obj
+                    }
+
+                    Write-Verbose "Closing connection to $computer"
+                    $session | Remove-CimSession
+                    $protocol = 'Stop'
+                }
+                catch {
+                    switch ($protocol) {
+                        'Wsman' {$protocol = 'Dcom'}
+                        'Dcom'  {$protocol = 'Stop'}
+                    }
+                }
+            }
+            until ($protocol = 'Stop')
+        }
+    }
+}
+
+function Get-HPComputer {
+    [cmdletbinding()]
+    param ()
+
+    $searchBase = 'OU=Gowen Field,OU=AFCONUSCENTRAL,OU=Bases,DC=AREA52,DC=AFNOAPPS,DC=USAF,DC=MIL'
+    $computers = (Get-ADComputer -Filter * -SearchBase $searchBase).Name
+
+    foreach ($computer in $computers) {
+        do {
+            Write-Verbose "Connecting to $computer on Wsman"
+            $protocol = 'Wsman'
+
+            try {
+                $option = New-CimSessionOption -Protocol $protocol
+                $sessionParams = @{
+                    ComputerName = $computer
+                    SessionOption = $option
+                    ErrorAction = 'Stop'
+                }
+                $session = New-CimSession @sessionParams
+
+                $gcimParams = @{
+                    ClassName = 'Win32_ComputerSystem'
+                    CimSession = $session
+                    Filter = "Manufacturer = 'HP'"
+                    Property = 'Manufacturer'
+                }
+                $gcim = Get-CimInstance @gcimParams
+
+                $obj = [pscustomobject] @{
+                    'ComputerName' = $gcim.psComputerName
+                    'Manufacturer' = $gcim.Manufacturer
+                }
+                Write-Output $obj
+
+                Write-Verbose "Closing connection to $computer"
+                $session | Remove-CimSession
+                $protocol = 'Stop'
+            }
+            catch {
+                switch ($protocol) {
+                    'Wsman' {$protocol = 'Dcom'}
+                    'Dcom'  {$protocol = 'Stop'}
+                }
+            }
+        }
+        until ($protocol = 'Stop')
     }
 }
